159f5e400cc8684f82dc323232b7c76a3d65e9d4
Johnny Ferraz <johnny.chr@hotmail.com>
2022-11-28 22:47:18 -0300

    adding withdraw and getting statements by date

--- src/index.js
+++ src/index.js
@@ -23,6 +23,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, [0])
+
+  return balance;
+}
+
 
 // Routes
 app.post("/account", (request, response) => {
@@ -68,5 +80,49 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send();
 })
 
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const  { customer } = request;
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => {
+    return statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  })
+
+  console.log(dateFormat, statement)
+
+  return response.json(statement);
+});
+
+app.put("/account", (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+})
 
 app.listen(3333);
